74bf1f78055f6d4682a3a7cf7e34f9ff7cdf6999
vurles <vurles27@gmail.com>
2019-06-18 16:42:31 -0400

    se crea modulo para importar todo lo de angular material para lo visual mas componente mapa para conectarme a la api de geolocalización

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './componente/mapa/mapa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
